db1b038c31b558613a7c285f0bca05bb3f6c0e07
PatrickCol <112722353+PatrickCol@users.noreply.github.com>
2025-06-22 13:54:04 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -8,6 +8,7 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -19,11 +20,23 @@ def lambda_handler(event, context):
     }
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=comentario)
+    DB_response = table.put_item(Item=comentario)
+
+    # Tambien vamos a guardar el comentario en un bucket de S3
+    s3 = boto3.client('s3')
+    bucket_name = os.environ["S3_BUCKET_NAME"]
+    S3_response = s3.put_object(
+        Bucket=bucket_name,
+        Key=f'comentarios/{tenant_id}/{uuidv1}.json',
+        Body=str(comentario),
+        ContentType='application/json'
+    )
+    
     # Salida (json)
     print(comentario)
     return {
         'statusCode': 200,
         'comentario': comentario,
-        'response': response
+        'DB_response': DB_response,
+        'S3_response': S3_response
     }
